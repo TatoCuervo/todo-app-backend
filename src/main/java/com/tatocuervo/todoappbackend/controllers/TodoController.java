66bf6e4a6713e7c27c06234ed16c11128214a6c5
Cristian Lopez <lopezcristianj@gmail.com>
2020-11-26 17:33:28 +1300

    Add Swagger

--- src/main/java/com/tatocuervo/todoappbackend/controllers/TodoController.java
+++ src/main/java/com/tatocuervo/todoappbackend/controllers/TodoController.java
@@ -3,12 +3,16 @@ package com.tatocuervo.todoappbackend.controllers;
 import com.tatocuervo.todoappbackend.model.Todo;
 import com.tatocuervo.todoappbackend.routes.Routes;
 import com.tatocuervo.todoappbackend.services.TodoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "Todos")
 @RestController
 @RequestMapping(path = Routes.TODO)
 public class TodoController {
@@ -16,23 +20,27 @@ public class TodoController {
     @Autowired
     private TodoService service;
 
+    @ApiOperation(value = "Get all TODOs by user")
     @GetMapping
     public List<Todo> getTodosByUser(@PathVariable String user) {
         return service.getTodosByUsername(user);
     }
 
-    @PostMapping
+    @ApiOperation(value = "Add todo by user")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> addTodoByUser(@PathVariable String user, @RequestBody Todo todo) {
         service.addTodoByUser(todo, user);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Update todo by id")
     @PutMapping(path = "/{id}")
     public ResponseEntity<Void> updateTodo(@PathVariable long id, @RequestBody Todo todo) {
         service.updateTodo(id, todo);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Delete todo by id")
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable long id) {
         service.deleteTodo(id);
